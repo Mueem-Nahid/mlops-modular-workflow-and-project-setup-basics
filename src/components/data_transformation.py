7204f10aae0f48d935fcebc44e18143d7c6da7e4
Mueem-Nahid <mueem51@gmail.com>
2026-03-16 16:10:41 +0600

    -  continue from 7. Data Transformation and Initiate the Feature Store 20.00

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -17,3 +17,15 @@ class DataTransformationConfig:
     preprocessor_obj_file_path = os.path.join('artifacts/data_transformation', 'preprocessor.pkl')
     feature_store_repo_path = "feature_repo"
 
+class DataTransformation:
+    def __init__(self):
+        try:
+            self.data_transformation_config = DataTransformationConfig()
+
+            repo_path = os.path.abspath(self.data_transformation_config.feature_store_repo_path)
+            os.makedirs(os.path.join(repo_path, "data"), exist_ok=True)
+
+            # continue from 7. Data Transformation and Initiate the Feature Store 20.00
+
+        except Exception as e:
+            raise CustomException(e, sys)
